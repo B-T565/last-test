bb5796a853b68b3697e0873c132f6704dcb82b8e
B-T565 <chheambunthoeun444@gmail.com>
2023-10-20 10:30:13 +0700

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { MenuComponent } from './components/menu/menu.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '/', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: MenuComponent }
 ];
