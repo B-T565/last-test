30a924efb741ac4b8c3778133e0510c682c12d4a
B-T565 <chheambunthoeun444@gmail.com>
2023-10-20 11:49:08 +0700

    try you code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ForgotComponent } from './components/forgot/forgot.component';
+// import { HeaderComponent } from './components/header/header.component';
+// import { ForgotComponent } from './components/forgot/forgot.component';
 
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   // {path: 'home', component: HomeComponent},
   {path:'login',component:LoginComponent},
-  {path: 'header',component: HeaderComponent},
-  {path: 'forgot',component: ForgotComponent}
+  // {path: 'header',component: HeaderComponent},
+  // {path: 'forgot',component: ForgotComponent}
 ];
 
 @NgModule({
